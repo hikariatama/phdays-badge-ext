ddeb98e83ba51fff771f00b75423c5fe2059a24e
Daniil Gazizullin <login@hikariatama.ru>
2025-05-27 00:52:58 +0300

    Pre-cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,57 +5,37 @@ const FILES_TO_CACHE = [
     './badge_white.png',
     './badge_red.png',
     './phdays-badge-pro.bin',
-    './fonts/InterDisplay-Italic.woff2',
-    './fonts/InterDisplay-BlackItalic.woff2',
-    './fonts/Inter-Medium.woff2',
-    './fonts/Inter-Black.woff2',
-    './fonts/InterDisplay-ExtraLightItalic.woff2',
-    './fonts/Inter-Regular.woff2',
-    './fonts/InterDisplay-ExtraBold.woff2',
-    './fonts/Inter-ExtraLight.woff2',
-    './fonts/inter.css',
-    './fonts/font.otf',
-    './fonts/InterDisplay-ExtraLight.woff2',
-    './fonts/Inter-ThinItalic.woff2',
-    './fonts/Inter-Light.woff2',
-    './fonts/InterDisplay-Bold.woff2',
-    './fonts/Inter-Thin.woff2',
-    './fonts/InterDisplay-ThinItalic.woff2',
-    './fonts/InterDisplay-Thin.woff2',
-    './fonts/Inter-MediumItalic.woff2',
-    './fonts/Inter-ExtraBold.woff2',
-    './fonts/InterDisplay-BoldItalic.woff2',
-    './fonts/Inter-SemiBold.woff2',
-    './fonts/Inter-LightItalic.woff2',
-    './fonts/InterDisplay-Regular.woff2',
-    './fonts/Inter-BoldItalic.woff2',
-    './fonts/Inter-Bold.woff2',
-    './fonts/InterVariable.woff2',
-    './fonts/Inter-ExtraBoldItalic.woff2',
-    './fonts/InterDisplay-Medium.woff2',
-    './fonts/InterDisplay-Black.woff2',
-    './fonts/Inter-Italic.woff2',
-    './fonts/InterDisplay-LightItalic.woff2',
-    './fonts/InterVariable-Italic.woff2',
-    './fonts/InterDisplay-SemiBold.woff2',
-    './fonts/InterDisplay-ExtraBoldItalic.woff2',
-    './fonts/Inter-ExtraLightItalic.woff2',
-    './fonts/InterDisplay-SemiBoldItalic.woff2',
-    './fonts/InterDisplay-Light.woff2',
-    './fonts/InterDisplay-MediumItalic.woff2',
-    './fonts/Inter-SemiBoldItalic.woff2',
-    './fonts/Inter-BlackItalic.woff2',
     'https://unpkg.com/@ffmpeg/core@0.12.9/dist/esm/ffmpeg-core.js',
     'https://unpkg.com/@ffmpeg/core@0.12.9/dist/esm/ffmpeg-core.wasm',
     'https://unpkg.com/@ffmpeg/core-mt@0.12.9/dist/umd/ffmpeg-core.worker.js',
 ];
 
+async function getFilesFromAutoindex(path) {
+    try {
+        const res = await fetch(path);
+        const text = await res.text();
+        const matches = [...text.matchAll(/href="([^"?][^"]+)"/g)];
+        return matches
+            .map(m => m[1])
+            .filter(href => href !== '../' && !href.endsWith('/'))
+            .map(href => path + href);
+    } catch (e) {
+        return [];
+    }
+}
+
 self.addEventListener('install', (event) => {
     self.skipWaiting();
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(FILES_TO_CACHE);
-        })
+        (async () => {
+            let filesToCache = [...FILES_TO_CACHE];
+            const fonts = await getFilesFromAutoindex('/fonts/');
+            const assets = await getFilesFromAutoindex('/assets/');
+            filesToCache = filesToCache.concat(fonts, assets);
+
+            const cache = await caches.open(CACHE_NAME);
+            await cache.addAll(filesToCache);
+        })()
     );
 });
 
